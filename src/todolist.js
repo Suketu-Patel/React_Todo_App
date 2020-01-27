605297296b3c147e474d0f51ec7bbad360cdb6db
Suketu Patel <suketu.patel@SuketuPatel.local>
2020-01-27 15:31:04 +0530

    initial commit

--- src/todolist.js
+++ src/todolist.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import Card from './cardList'
-var data = require("./todos.json")
+import Card from './cardList';
+import axios from 'axios';
+
 class ToDoList extends React.Component {
     constructor(props) {
         super(props);
@@ -9,9 +9,15 @@ class ToDoList extends React.Component {
             cards: [{
                 id: 0,
                 subject: "",
-                todos: [data]
+                todos: [
+                    {
+                        id: 0,
+                        text: "Sammple to-do",
+                        completed: false
+                    }
+                ]
             }],
-            val: "",
+            val: ""
         }
         this.handleClick = this.handleClick.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
@@ -22,42 +28,73 @@ class ToDoList extends React.Component {
         this.subjectChange = this.subjectChange.bind(this);
         this.handleCardRemove = this.handleCardRemove.bind(this);
     }
-    handleEditChange(e, id, lid) {
-        const list = [...this.state.cards[id].todos];
-        const eNode = list.filter(item => item.id === lid);
+    storeResult() {
+        axios.post("http://localhost:3005/store", { state: this.state }).then(res => {
+        })
+    }
+    handleEditChange(e, c_id, lid) {
+        console.log("handleEditChange")
+        const list = [...this.state.cards];
+        const cNode = list.filter(item => item.id === c_id);
+        const list2 = [...cNode[0].todos];
+        const eNode = list2.filter(item => item.id === lid);
         eNode[0].text = e.target.value;
         this.setState(this.state);
+        this.storeResult()
     }
+    handleRemove(c_id, lid) {
+        console.log("handleRemove")
+        const list = [...this.state.cards];
+        const cNode = list.filter(item => item.id === c_id);
+        const list2 = [...cNode[0].todos];
+        const updatedList = list2.filter(item => item.id !== lid);
+        // console.log("State",list)
+        // this.setState((state) => cNode[0].todos = updatedList);
+        cNode[0].todos = updatedList;
+        this.setState(this.state);
+        this.storeResult()
 
-    handleRemove(card_id, id) {
-        const list = [...this.state.cards[card_id].todos];
-        const updatedList = list.filter(item => item.id !== id);
-        this.setState((state) => state.cards[card_id].todos = updatedList);
     }
-    handleCardRemove(card_id){
-        console.log(this.state.cards.length)
-        if(this.state.cards.length === 1 ){
+    handleCardRemove(card_id) {
+        console.log("handleCardRemove")
+        if (this.state.cards.length === 1) {
             this.setState({
                 cards: [{
                     id: 0,
                     subject: "",
-                    todos: [data]
+                    todos: [{
+                        "id": 0,
+                        "text": "Sample to-do",
+                        "completed": false
+                    }]
                 }],
                 val: "",
             })
-        }else{
-            const list = [...this.state.cards];
+        } else {
+            const list = this.state.cards;
             const updatedList = list.filter(item => item.id !== card_id);
-            this.setState((state) => state.cards = updatedList);
+            // console.log(updatedList)
+            this.setState({
+                cards:updatedList
+            });
         }
+        this.storeResult()
     }
     handleClick(li, card_id) {
-        const list = [...this.state.cards[card_id].todos];
-        const checkedNode = list.filter(item => item.id === li.id);
+        console.log("handleClick")
+        const list = [...this.state.cards];
+        const toDoList = list.filter(item => item.id === card_id);
+        console.log(toDoList,li.id)
+        const checkedNode = toDoList[0].todos.filter(item => item.id === li.id);
+        console.log(checkedNode)
         checkedNode[0].completed = !checkedNode[0].completed;
-        this.setState(this.state);    
+        this.setState(this.state);
+        this.storeResult()
+
     }
     handleAdd(e) {
+        console.log("handleAdd");
+        
         if (e.key === 'Enter') {
             let previd;
             if (this.state.cards.length === 0) {
@@ -68,47 +105,82 @@ class ToDoList extends React.Component {
             if (this.state.val === "") {
                 return;
             }
-            this.setState({
-                cards: this.state.cards.concat([
-                    { id: previd + 1, subject: this.state.val, todos: [] }
-                ])
-            })
+            this.setState((state) =>
+                state.cards.push(
+                    {
+                        id: previd + 1,
+                        subject: this.state.val,
+                        todos: []
+                    }
+                )
+            )
         } else {
             return;
         }
+        console.log("FIRED")
         this.setState({
             val: ""
         })
+        console.log("Storing")
+        this.storeResult()
         document.querySelector(".inputL").blur();
+
     }
     handleTodoAdd(c_id) {
+        console.log("handleTodoAdd");
         let previd;
-        if (this.state.cards[c_id].todos.length === 0) {
+        const list = [...this.state.cards];
+        const eNode = list.filter(item => item.id === c_id);
+        if (eNode[0].todos.length === 0) {
             previd = 0;
         } else {
-            previd = this.state.cards[c_id].todos[this.state.cards[c_id].todos.length - 1].id
+            previd = eNode[0].todos[eNode[0].todos.length - 1].id
         }
-        this.setState((state) => state.cards[c_id].todos.push({
+        eNode[0].todos.push({
             id: previd + 1,
             text: "",
             completed: false
-        }))
+        })
+        this.setState(this.state);
+        this.storeResult()
+
     }
-    subjectChange(event, card_id) {
-        const prevText = this.state.cards[card_id];
-        prevText.subject = event.target.value;
-        this.setState(this.state)
-    }
+
     handleChange(event) {
+        console.log("handleChange");
         this.setState({ val: event.target.value })
     }
+
+    componentDidMount() {
+        axios.get("http://localhost:3005/todos.json").then((res) => {
+            this.setState(
+                res.data
+            )
+            console.log("Mounted")
+        })
+    }
+
+    subjectChange(event, card_id) {
+        console.log("subjectChange");
+        const list = this.state.cards;
+        const eNode = list.filter(item => item.id === card_id);
+        eNode[0].subject = event.target.value;
+        this.setState(this.state)
+        this.storeResult()
+
+    }
+    subjectHandler(cid) {
+        console.log("subjectHandler");
+        const list = [...this.state.cards];
+        const eNode = list.filter(item => item.id === cid);
+        return eNode[0].subject;
+    }
     render() {
-        // console.log(this.state.cards.length)
-        console.log("state",this.state)
+        console.log("--------------------")
         const cardlist = this.state.cards.map((card) => {
             return (
                 <Card key={card.id}
-                    value={this.state.cards[card.id].subject}
+                    value={this.subjectHandler(card.id)}
                     card_id={card.id}
                     list={card.todos}
                     subjectChange={this.subjectChange}
@@ -116,7 +188,7 @@ class ToDoList extends React.Component {
                     handleEditChange={this.handleEditChange}
                     handleTodoAdd={this.handleTodoAdd}
                     handleRemove={this.handleRemove}
-                    handleCardRemove = {this.handleCardRemove}
+                    handleCardRemove={this.handleCardRemove}
                 />
             )
         });
